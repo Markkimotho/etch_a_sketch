367716833b8dc5f748b38ac32e74e1eb05b591b9
MarkTinega <martinozas@gmail.com>
2023-06-10 12:22:03 +0300

    Added buttons, and made the UI a little bit more appealing

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-const gridSize = 100; // Change this value to adjust the grid size
+const randomColorBtn = document.querySelector(".random-color");
+
+const gridSize = 10; // Change this value to adjust the grid size
+bgColor = 'green';
+
+
+
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min)) + min;
+    return num;
+}
+
+function randomColor(){
+    return `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
+}
 
 // Get the container element
-const container = document.querySelector('.container');
+const container = document.getElementById('container');
 
 // Set the grid size as a CSS variable
 container.style.setProperty('--grid-size', gridSize);
 
+
 // Create the grid squares
-function createGrid(gridSize) {
+function createGrid(gridSize){
     for (let i = 0; i < gridSize * gridSize; i++) {
-    const square = document.createElement('div');
-    square.classList.add('grid-square');
-    container.appendChild(square);
+        const square = document.createElement('div');
+        square.classList.add('grid-square');
+        container.appendChild(square);
+
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = randomColor();
+    });
     }
 }
-
 createGrid(gridSize);
+
+function clearGrid() {
+    return;
+}
