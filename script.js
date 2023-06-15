18f5e34707736c793e504f80a64692a4ed46c3ba
MarkTinega <martinozas@gmail.com>
2023-06-15 16:56:44 +0300

    Added functionality to the buttons, created a slider, and set the base for final production

--- script.js
+++ script.js
@@ -1,40 +1,81 @@
+const colorPicker = document.getElementById("color-picker");
 const randomColorBtn = document.querySelector(".random-color");
+const eraserBtn = document.querySelector(".eraser");
+const clearBtn = document.querySelector(".clear");
+const slider = document.getElementById("slider");
+let gridSize = 16; // Default grid size
+let defaultColor = 'black';
 
-const gridSize = 10; // Change this value to adjust the grid size
-bgColor = 'green';
-
-
-
-function random(min, max) {
-    const num = Math.floor(Math.random() * (max - min)) + min;
-    return num;
+/* Random Color Generator */
+function generateRandomColor() {
+  const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+  const red = random(0, 255);
+  const green = random(0, 255);
+  const blue = random(0, 255);
+  return `rgb(${red}, ${green}, ${blue})`;
 }
 
-function randomColor(){
-    return `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
-}
-
-// Get the container element
 const container = document.getElementById('container');
 
-// Set the grid size as a CSS variable
-container.style.setProperty('--grid-size', gridSize);
+// Function to create the grid squares
+function createGrid() {
+  container.innerHTML = ''; // Clear the container
 
+  container.style.setProperty('--grid-size', gridSize);
 
-// Create the grid squares
-function createGrid(gridSize){
-    for (let i = 0; i < gridSize * gridSize; i++) {
-        const square = document.createElement('div');
-        square.classList.add('grid-square');
-        container.appendChild(square);
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const square = document.createElement('div');
+    square.classList.add('grid-square');
+    container.appendChild(square);
 
     square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = randomColor();
+      square.style.backgroundColor = defaultColor;
     });
-    }
+  }
 }
-createGrid(gridSize);
 
-function clearGrid() {
-    return;
-}
+// Initial grid creation
+createGrid();
+
+eraserBtn.addEventListener("click", () => {
+  const squares = document.querySelectorAll(".grid-square");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "";
+    });
+  });
+});
+
+clearBtn.addEventListener("click", () => {
+  const squares = document.querySelectorAll('.grid-square');
+  squares.forEach((square) => {
+    square.style.backgroundColor = "";
+  });
+});
+
+randomColorBtn.addEventListener("click", () => {
+  const squares = document.querySelectorAll(".grid-square");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = generateRandomColor();
+    });
+  });
+});
+
+colorPicker.addEventListener("input", (event) => {
+  const selectedColor = event.target.value;
+  const squares = document.querySelectorAll('.grid-square');
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = selectedColor;
+    });
+  });
+});
+
+slider.addEventListener("input", (event) => {
+  gridSize = event.target.value;
+  const gridSizeValue = document.querySelector(".gridSizeValue")
+  gridSizeValue.textContent = `Size: ${gridSize} x ${gridSize}`
+  gridSizeValue.style.fontWeight = "bold";
+  createGrid(); // Recreate the grid with the new size
+});
